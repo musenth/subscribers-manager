28a32cb7fcc6e92a111cb74de23a121f17ab1691
Peter <mckinleypt@gmail.com>
2018-09-24 23:52:51 +0700

    Initial commit

---
+++ src/main/kotlin/com/interminable/subscribers/manager/App.kt
@@ -0,0 +1,11 @@
+package com.interminable.subscribers.manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SubscribersManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SubscribersManagerApplication>(*args)
+}
